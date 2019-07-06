24d6ee2566b0b9b28aecc338015a667d8eb86828
Anubhav1425 <52591590+Anubhav1425@users.noreply.github.com>
2019-07-06 12:00:56 +0530

    Add files via upload

---
+++ greatestofnumbers.py
@@ -0,0 +1,9 @@
+a=int(input('enter first number'))
+b=int(input('enter sec number'))
+c=int(input('enter third number'))
+if a>b and a>c :	
+	print('first number is largest=',a)
+elif b>c:
+	print('sec number is largest=',b)
+else:
+	print ('third number is largest=',c)
