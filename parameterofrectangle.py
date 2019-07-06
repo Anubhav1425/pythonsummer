9e6a9af40f03b19ab44d6f7a73989d862fffb564
Anubhav1425 <52591590+Anubhav1425@users.noreply.github.com>
2019-07-06 11:10:57 +0530

    fourth commit

---
+++ parameterofrectangle.py
@@ -0,0 +1,7 @@
+a=int(input('enter the length of first side='))
+b=int(input('enter the length of second side='))
+if a==b:	
+	print('not a rectangle')
+else:
+	c=2*(a+b)
+	print('parameter of rectangle is',c)
