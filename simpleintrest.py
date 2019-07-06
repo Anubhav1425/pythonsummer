16d72b611c03fff5a04d45dc9d1fc956557e428e
Anubhav1425 <52591590+Anubhav1425@users.noreply.github.com>
2019-07-06 11:21:11 +0530

    fourthcommit

---
+++ simpleintrest.py
@@ -0,0 +1,7 @@
+a= float(input('enter principal='))
+b=float(input('enter rate='))
+c=float(input('enter Time='))
+si=(a*b*c)/100
+print('simple intrest is=',si)
+ti=a+si
+print('Total amounnt=',ti)
