fc7339ffa336141f2943d9537a586296d4caafaa
Anubhav1425 <52591590+Anubhav1425@users.noreply.github.com>
2019-07-06 11:46:04 +0530

    Add files via upload

---
+++ distancebtwtwopoints.py
@@ -0,0 +1,9 @@
+a=float(input('enter x1='))
+b=float(input('enter x2='))
+c=float(input('enter y1='))
+d=float(input('enter y2='))
+e=(b-a)**2
+f=(d-c)**2
+g=(e+f)**(1/2)
+print('distance between two points=',g)
+
