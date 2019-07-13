43fcc74cb36a7838401ce033d7e3b25ab5658ad0
Anubhav1425 <52591590+Anubhav1425@users.noreply.github.com>
2019-07-13 14:30:16 +0530

    Add files via upload

---
+++ stringlen.py
@@ -0,0 +1,2 @@
+s="anubhav"
+print(len(s))
---
+++ stringreverse.py
@@ -0,0 +1,5 @@
+s = "My name Anubhav"
+print ("The original string  is : ",end="") 
+print (s) 
+print ("The reversed string(using stack) is : ",end="") 
+print (s[::-1]) 
